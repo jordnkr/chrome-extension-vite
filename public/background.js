395bd601807405795d0071e737b35442c5431c00
jordnkr <jordnkr408@gmail.com>
2023-04-10 21:52:31 -0500

    some stuff

--- public/background.js
+++ public/background.js
@@ -4,3 +4,19 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+const ports = new Set();
+
+chrome.runtime.onConnect.addListener((port) => {
+  ports.add(port);
+
+  port.onDisconnect.addListener(() => {
+    ports.delete(port);
+  });
+});
+
+chrome.debugger.onDetach.addListener((source, reason) => {
+  ports.forEach((port) => {
+    port.postMessage({ action: "updateCheckbox", checked: false });
+  });
+});
--- src/js/panel.js
+++ src/js/panel.js
@@ -8,35 +8,65 @@ chrome.devtools.panels.create(
   function (panel) {}
 );
 
-const color = Color("#632169");
-const heading = document.getElementById("myHeading");
-heading.style.background = color.hex();
+// const color = Color("#632169");
+// const heading = document.getElementById("myHeading");
+// heading.style.background = color.hex();
 
-document.getElementById("protanopiaBtn").addEventListener("click", async () => {
+const protanopiaCheckbox = document.getElementById("protanopiaCheckbox");
+
+protanopiaCheckbox.addEventListener("change", toggleProtanopia);
+
+async function toggleProtanopia(event) {
   const tab = await chrome.tabs.get(chrome.devtools.inspectedWindow.tabId);
   const tabId = tab.id;
   const debuggeeId = { tabId };
-  chrome.debugger.attach(debuggeeId, "1.3", () => {
-    if (chrome.runtime.lastError) {
-      console.log(
-        "runtime.lastError",
-        tab.id,
-        chrome.runtime.lastError.message
-      );
-      return;
-    }
-    chrome.debugger.sendCommand(
-      debuggeeId,
-      "Emulation.setEmulatedVisionDeficiency",
-      {
-        type: "protanopia",
-      },
-      (result) => {
-        if (chrome.runtime.lastError) {
-          console.log(chrome.runtime.lastError.message);
-        }
-        console.log("Command sent:", JSON.stringify(result));
+
+  if (event.target.checked) {
+    chrome.debugger.attach(debuggeeId, "1.3", () => {
+      if (chrome.runtime.lastError) {
+        console.log("runtime.lastError", tab.id, chrome.runtime.lastError.message);
+        return;
       }
-    );
-  });
+      chrome.debugger.sendCommand(
+        debuggeeId,
+        "Emulation.setEmulatedVisionDeficiency",
+        { type: "protanopia" },
+        (result) => {
+          if (chrome.runtime.lastError) {
+            console.log(chrome.runtime.lastError.message);
+          }
+          console.log("Command sent:", JSON.stringify(result));
+        }
+      );
+    });
+  } else {
+    chrome.debugger.detach(debuggeeId);
+  }
+}
+
+const port = chrome.runtime.connect({ name: "devtools" });
+
+port.onMessage.addListener((message) => {
+  if (message.action === "updateCheckbox") {
+    protanopiaCheckbox.removeEventListener("change", toggleProtanopia);
+    protanopiaCheckbox.checked = message.checked;
+    protanopiaCheckbox.addEventListener("change", toggleProtanopia);
+  }
 });
+
+
+
+// chrome.tabs.onActivated.addListener(function(closedTabId, removeInfo) {
+//       chrome.debugger.detach({tabId: tabId}, function() {
+//         console.log('Tab closed. Debugger detached.');
+//       });
+//   });
+
+// Listen for tab change and page reload events
+// chrome.tabs.onRemoved.addListener(function(closedTabId, removeInfo) {
+//   if (closedTabId === tabId) {
+//     chrome.debugger.detach({tabId: tabId}, function() {
+//       console.log('Tab closed. Debugger detached.');
+//     });
+//   }
+// });
