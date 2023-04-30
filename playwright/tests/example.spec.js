051eedba59242ecd1cc102b27ba6d13faa6016bd
jordnkr <jordnkr408@gmail.com>
2023-04-30 14:58:11 -0500

    code organization

--- playwright/tests/example.spec.js
+++ playwright/tests/example.spec.js
@@ -1,62 +1,17 @@
-const { test: base, chromium, webkit, expect } = require('@playwright/test')
-const path = require('path')
+import { test, expect } from "./fixtures.js";
 
-const extensionPath = path.join(__dirname, '../../dist') // make sure this is correct
+test("example test", async ({ page }) => {
+  await page.goto("https://example.com");
+  //await page.waitForTimeout(30000); // this is here so that it won't automatically close the browser window
+});
 
-const test = base.extend({
-  context: async ({ browserName }, use) => {
-    const browserTypes = { chromium, webkit }
-    const launchOptions = {
-      devtools: true,
-      headless: false,
-      args: [
-        `--disable-extensions-except=${extensionPath}`
-      ],
-      viewport: {
-        width: 1920,
-        height: 1080
-      }
-    }
-    const context = await browserTypes[browserName].launchPersistentContext(
-      '',
-      launchOptions
-    )
+test("extension page", async ({ page, context, extensionId }) => {
+  await page.goto(`chrome-extension://${extensionId}/index.html`);
 
-    await context.addInitScript(() => {
-      window.__testmode = true;
-    });
+  const testedPage = await context.newPage();
+  await testedPage.goto("http://jordnkr.github.io/cssnippets/");
 
-    await context.pages()[0].close();
-
-
-    await use(context)
-    await context.close()
-  },
-  extensionId: async ({ context }, use) => {
-    // for manifest v3:
-    let [background] = context.serviceWorkers();
-    if (!background)
-      background = await context.waitForEvent('serviceworker');
-
-    const extensionId = background.url().split('/')[2];
-    await use(extensionId);
-  },
-})
-
-test.describe('Popup', () => {
-  test('example test', async ({ page }) => {
-    await page.goto('https://example.com');
-    await page.waitForTimeout(30000) // this is here so that it won't automatically close the browser window
-  });
-  
-  test('extension page', async ({ page, context, extensionId }) => {
-    await page.goto(`chrome-extension://${extensionId}/index.html`);
-
-    const testedPage = await context.newPage();
-    await testedPage.goto('http://jordnkr.github.io/cssnippets/');
-
-    await page.click("#protanopiaBtn");
-    await expect(page.locator('#result')).toHaveText('0');
-    await page.waitForTimeout(30000) // this is here so that it won't automatically close the browser window
-  });
-})
+  await page.click("#protanopiaBtn");
+  await expect(page.locator("#result")).toHaveText("0");
+  await page.waitForTimeout(10000); // this is here so that it won't automatically close the browser window
+});
--- playwright/tests/fixtures.js
+++ playwright/tests/fixtures.js
@@ -1,32 +1,41 @@
 import { test as base, expect, chromium } from "@playwright/test";
 import path from "path";
 
-export const test =
-  base.extend <
-  {
-    context,
-    extensionId,
-  } >
-  {
-    context: async ({}, use) => {
-      const pathToExtension = path.join(__dirname, "../../dist");
-      const context = await chromium.launchPersistentContext("", {
-        headless: false,
-        args: [
-          `--disable-extensions-except=${pathToExtension}`,
-          `--load-extension=${pathToExtension}`,
-        ],
-      });
-      await use(context);
-      await context.close();
-    },
-    extensionId: async ({ context }, use) => {
-      // for manifest v3:
-      let [background] = context.serviceWorkers();
-      if (!background) background = await context.waitForEvent("serviceworker");
+const extensionPath = path.join(__dirname, "../../dist"); // make sure this is correct
 
-      const extensionId = background.url().split("/")[2];
-      await use(extensionId);
-    },
-  };
-export const expect2 = test.expect;
+exports.test = base.extend({
+  context: async ({ browserName }, use) => {
+    const browserTypes = { chromium };
+    const launchOptions = {
+      //devtools: true,
+      headless: false,
+      args: [`--disable-extensions-except=${extensionPath}`],
+      // viewport: {
+      //   width: 1920,
+      //   height: 1080,
+      // },
+    };
+    const context = await browserTypes[browserName].launchPersistentContext(
+      "",
+      launchOptions
+    );
+
+    await context.addInitScript(() => {
+      window.__testmode = true;
+    });
+
+    await context.pages()[0].close();
+
+    await use(context);
+    await context.close();
+  },
+  extensionId: async ({ context }, use) => {
+    // for manifest v3:
+    let [background] = context.serviceWorkers();
+    if (!background) background = await context.waitForEvent("serviceworker");
+
+    const extensionId = background.url().split("/")[2];
+    await use(extensionId);
+  },
+});
+exports.expect = expect;
