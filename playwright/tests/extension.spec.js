655978a2b251422e467b06cbb3dbebff394be479
jordnkr <jordnkr408@gmail.com>
2023-05-02 15:22:04 -0500

    Further a11y test improvements

--- playwright/tests/extension.spec.js
+++ playwright/tests/extension.spec.js
@@ -1,19 +1,18 @@
 import { test, expect } from "../fixtures/test-fixture.js";
 import ViewOnePage from "../pages/view-one-page.js";
 import ViewTwoPage from "../pages/view-two-page.js";
-const AxeBuilder = require("@axe-core/playwright").default;
 
 // pages
 let viewOnePage;
 let viewTwoPage;
-let testedPage;
+let extensionTarget;
 
 test.beforeEach(async ({ page, context, extensionId }) => {
   viewOnePage = new ViewOnePage(page);
   viewTwoPage = new ViewTwoPage(page);
 
-  testedPage = await context.newPage();
-  await testedPage.goto("http://jordnkr.github.io/cssnippets/");
+  extensionTarget = await context.newPage();
+  await extensionTarget.goto("http://jordnkr.github.io/cssnippets/");
 
   await page.goto(`chrome-extension://${extensionId}/index.html`);
 });
@@ -21,7 +20,7 @@ test.beforeEach(async ({ page, context, extensionId }) => {
 test("extension view 1 page", async ({ page }) => {
   await viewOnePage.clickProtonopiaButton();
   await expect(await viewOnePage.getResultText()).toBe("0");
-  await page.waitForTimeout(5000); // this is here so that it won't automatically close the browser window
+  //await page.waitForTimeout(5000); // this is here so that it won't automatically close the browser window
 });
 
 test("extension view 2 page", async () => {
@@ -32,23 +31,4 @@ test("extension view 2 page", async () => {
   await expect(viewTwoPage.output).toContainText("Name: test");
   await expect(viewTwoPage.output).toContainText("Phone: 123");
   //await page.waitForTimeout(15000); // this is here so that it won't automatically close the browser window
-});
-
-test("validate View 1 with axe-core", async ({ page }) => {
-  const axeResults = await new AxeBuilder({ page })
-    .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"]) // Specify the WCAG standards
-    .analyze();
-
-  expect(axeResults.violations).toEqual([]);
-});
-
-test("validate View 2 with axe-core", async ({ page }) => {
-  await viewOnePage.tabs.clickViewTwo();
-
-  const axeResults = await new AxeBuilder({ page })
-    .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"]) // Specify the WCAG standards
-    .analyze();
-
-  //expect(axeResults.violations).toEqual([]);
-  expect(axeResults.violations).toHaveLength(0);
 });
