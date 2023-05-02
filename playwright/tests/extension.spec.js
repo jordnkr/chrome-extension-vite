d4ecf59b883d557d0fc06a86da6c36cfb03ace40
jordnkr <jordnkr408@gmail.com>
2023-05-01 21:42:54 -0500

    Adding a11y tests and fixing a11y issues

--- playwright/tests/extension.spec.js
+++ playwright/tests/extension.spec.js
@@ -1,16 +1,18 @@
 import { test, expect } from "../fixtures/test-fixture.js";
 import ViewOnePage from "../pages/view-one-page.js";
 import ViewTwoPage from "../pages/view-two-page.js";
+const AxeBuilder = require("@axe-core/playwright").default;
 
 // pages
 let viewOnePage;
 let viewTwoPage;
+let testedPage;
 
 test.beforeEach(async ({ page, context, extensionId }) => {
   viewOnePage = new ViewOnePage(page);
   viewTwoPage = new ViewTwoPage(page);
 
-  const testedPage = await context.newPage();
+  testedPage = await context.newPage();
   await testedPage.goto("http://jordnkr.github.io/cssnippets/");
 
   await page.goto(`chrome-extension://${extensionId}/index.html`);
@@ -24,10 +26,29 @@ test("extension view 1 page", async ({ page }) => {
 
 test("extension view 2 page", async () => {
   await viewOnePage.tabs.clickViewTwo();
-  await viewTwoPage.enterName('test');
-  await viewTwoPage.enterPhone('123');
+  await viewTwoPage.enterName("test");
+  await viewTwoPage.enterPhone("123");
   await viewTwoPage.clickSubmit();
-  await expect(viewTwoPage.output).toContainText('Name: test');
-  await expect(viewTwoPage.output).toContainText('Phone: 123');
+  await expect(viewTwoPage.output).toContainText("Name: test");
+  await expect(viewTwoPage.output).toContainText("Phone: 123");
   //await page.waitForTimeout(15000); // this is here so that it won't automatically close the browser window
+});
+
+test("validate View 1 with axe-core", async ({ page }) => {
+  const axeResults = await new AxeBuilder({ page })
+    .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"]) // Specify the WCAG standards
+    .analyze();
+
+  expect(axeResults.violations).toEqual([]);
+});
+
+test("validate View 2 with axe-core", async ({ page }) => {
+  await viewOnePage.tabs.clickViewTwo();
+
+  const axeResults = await new AxeBuilder({ page })
+    .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"]) // Specify the WCAG standards
+    .analyze();
+
+  //expect(axeResults.violations).toEqual([]);
+  expect(axeResults.violations).toHaveLength(0);
 });
